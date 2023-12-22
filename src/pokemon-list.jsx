1141cff68989ca5b72f096da93c97d2a431a38b6
kihno <stistyle13@gmail.com>
2023-12-21 16:59:53 -0800

    Connect to pokeapi

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -2,9 +2,34 @@
 // Make sure to replace limit and offset with the appropriate values
 // https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
 
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const PokemonList = () => {
+  const [pokemons, setPokemon] = useState([]);
+  const [count, setCount] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=5&offset=0')
+      .then((response) => {
+        console.log(response.data);
+        setPokemon(response.data.results);
+        setCount(response.data.count);
+      });
+  }, []);
+
   return (
-    <div>Pokemon</div>
+    <div>
+      <ul>
+        {pokemons.map(pokemon => (
+          <li key={pokemon.name}>
+            {pokemon.name}
+          </li>
+        ))}
+      </ul>
+      <div>Displaying {pokemons.length} of {count} results </div>
+      <button>Load more</button>
+    </div>
   )
 };
 
