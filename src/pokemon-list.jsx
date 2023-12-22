15ae702fe3cf4ccdede6183b78b6740ea1ce6adb
kihno <stistyle13@gmail.com>
2023-12-21 16:39:22 -0800

    Initial commit

---
+++ src/pokemon-list.jsx
@@ -0,0 +1,11 @@
+// You can retrieve the pokemons by calling the following API
+// Make sure to replace limit and offset with the appropriate values
+// https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
+
+const PokemonList = () => {
+  return (
+    <div>Pokemon</div>
+  )
+};
+
+export default PokemonList;
